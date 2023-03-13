b39da58a97ee952cf818caeb4e55afc95124757c
Galaa <galbadrakhtserenkhand@gmail.com>
2023-03-13 11:15:06 +0800

    03.13 ugluu update 8

--- resources/views/site/users/partials/section.blade.php
+++ resources/views/site/users/partials/section.blade.php
@@ -1,8 +1,9 @@
+{{ Request::segment(1) }}
 <nav class="content-filter">
   <div class="container">
     <a href="#" class="content-filter__toggle"></a>
     <ul class="content-filter__list">
-      <li class="content-filter__item {{ }}"><a href="{{ route('users.index') }}" class="content-filter__link">{{ __("site.tp_personal_info") }}</a></li>
+      <li class="content-filter__item"><a href="{{ route('users.index') }}" class="content-filter__link">{{ __("site.tp_personal_info") }}</a></li>
       <li class="content-filter__item content-filter__item--active"><a href="{{ route('users.idcard') }}" class="content-filter__link">{{ __("site.tp_idcard") }}</a></li>
       <li class="content-filter__item "><a href="{{ route('users.contract') }}" class="content-filter__link">{{ __("site.tp_contract") }}</a></li>
       <li class="content-filter__item "><a href="{{ route('users.finish') }}" class="content-filter__link">{{ __("site.tp_registred") }}</a></li>
